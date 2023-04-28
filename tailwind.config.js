0eca26f0d955615177c4f56bb7f939edf55738af
Caio Detz <89801456+kard-py@users.noreply.github.com>
2023-04-28 13:52:17 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,18 @@ module.exports = {
     extend: {
      animation: {
         'open': 'open .5s linear forwards',
+        'close': 'open .5s linear forwards',
       },
       keyframes: {
         open: {
           '0%': { width: '0%' },
           '100%': { width: '100%' },
-        }
+        },
+        open: {
+          '0%': { width: '100%' },
+          '100%': { width: '0%' },
+        },
+        
       }
     },
   },
