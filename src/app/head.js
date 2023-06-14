e4c7a6e3e29637e5e4fb06a9987c467e65b94da2
CaioDetz <caiodetz654@gmail.com>
2023-06-14 20:23:40 +0000

    UP

--- src/app/head.js
+++ src/app/head.js
@@ -1,7 +1,31 @@
 export default function Head() {
   return (
     <>
+      {/* <!-- HTML Meta Tags --> */}
       <title>Caio Detz</title>
+      <meta name="description" content="Desenvolvedor Front-End & Back-End" />
+
+      {/* <!-- Facebook Meta Tags --> */}
+      <meta property="og:url" content="https://caiodetz.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Caio Detz" />
+      <meta
+        property="og:description"
+        content="Desenvolvedor Front-End & Back-End"
+      />
+      <meta property="og:image" content="https://caiodetz.com/CaioDetz.png" />
+
+      {/* <!-- Twitter Meta Tags --> */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="caiodetz.com" />
+      <meta property="twitter:url" content="https://caiodetz.com" />
+      <meta name="twitter:title" content="Caio Detz" />
+      <meta
+        name="twitter:description"
+        content="Desenvolvedor Front-End & Back-End"
+      />
+      <meta name="twitter:image" content="https://caiodetz.com/CaioDetz.png" />
+
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Caio Detz Dev" />
       <link rel="icon" href="/favicon.ico" />
