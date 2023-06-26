cdc0c39ea871b31ca5d325c31ebc66ba9a160edf
Caio Detz <89801456+kard-py@users.noreply.github.com>
2023-06-26 19:50:19 +0000

    UPDATE HOME

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 import Card from "@/components/Card";
+import projeto from "../../public/projeto1.png";
+import Image from "next/image";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiTailwindcss } from "react-icons/si";
+import { DiMongodb } from "react-icons/di";
+
 export default function Home() {
   return (
-    <main className="flex flex-1 h-full flex-wrap justify-center items-center w-full">
-      <Card className="flex flex-col items-center"></Card>
-    </main>
+    <main className="flex flex-1 h-full flex-wrap justify-center items-center w-full"></main>
   );
 }
