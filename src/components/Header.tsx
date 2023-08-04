db6dc2a4a1609ee1a0e38b50c28c797a67900a11
Caio Detz <89801456+kard-py@users.noreply.github.com>
2023-08-04 15:02:43 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ export default function Header() {
   return (
     <header className="flex w-full items-center justify-between py-14 mb-52">
       <div className="text-2xl font-bold text-black">C</div>
-      <nav className="md:flex hidden gap-16">
+      <nav className="xl:flex hidden gap-16">
         <div className="text-xl font-bold text-black cursor-pointer">Home</div>
         <div className="text-xl font-regular cursor-pointer">Trabalhos</div>
         <div className="text-xl font-regular cursor-pointer">Sobre</div>
         <div className="text-xl font-regular cursor-pointer">Contato</div>
       </nav>
-      <nav className="md:hidden flex w-7 h-7">
+      <nav className="xl:hidden flex w-7 h-7">
         <Image
           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAtUlEQVR4nO3aMQoEQRACQP//6b54YcM9NKgCcxk7nAQAAAAAAPiDkyy9Qb2AxCA3fAj1AhKD3PAh1AtIDHLDh1AvIDHIDR9CvYDkOQgAAAAAAAAA8M4ng0y9Qb2AxCA3fAj1AhKD3PAh1AtIDHLDh1AvIDHIDR9CvYDkOQgAAAAAAAAA8M4ng0y9Qb2AxCA3fAj1AhKD3PAh1AtIDHLDh1AvIDHIDR9CvYDkOQgAAAAAAJAv/QDM2azukFX6EgAAAABJRU5ErkJggg=="
           alt="MenuIcon"
