7f7a6414851a60ce454072fa5d60f229c0ff25b5
CaioDetz <caiodetz654@gmail.com>
2023-08-07 14:59:51 +0000

    UP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,12 @@ export default function Home() {
         <h1 className="text-4xl">Olá.</h1>
         <h1 className="text-4xl font-semibold">Eu Sou Caio Detz.</h1>
         <span className="text-4xl">
-          Designer e Prgramador da equipe Harpia.
+          Designer e Programador da equipe Harpia.
         </span>
         <span className="text-4xl">Com sede em Sorriso, Brasil.</span>
 
         <p className="md:w-[50rem] mt-24 text-2xl">
-          Com mais de 13 anos de experiência em programação, fundei a Harpia,
+          Com mais de 7 anos de experiência em programação, fundei a Harpia,
           uma empresa voltada para o ramo de desenvolvimento de software.
           Algumas das minhas experiências incluem o uso de frameworks como
           React, React-Native, NextJs, Flutter, entre outros.
